d759e9b035584500900bcc032471ac5b91d32052
Konstantin Perikov <pericov.ko@gmail.com>
2017-07-04 23:28:06 +0300

    update the project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
-}
+extern crate iron;
+extern crate mime;
+
+use iron::prelude::*;
+use iron::status;
+
+	fn main() {
+		println!("Serving on http://localhost:3000...");
+		Iron::new(get_form).http("localhost:3000").unwrap();
+	}
+
+	#[allow(unused_variables)]
+	fn get_form(request: &mut Request) -> IronResult<Response> {
+		let mut response = Response::new();
+		response.set_mut(status::Ok);
+		response.set_mut(mime::TEXT_HTML);
+		response.set_mut(r#"
+			<title>GCD Calculator</title>
+			<form action="/gcd" method="post">
+			<input type="text" name="n"/>
+			<input type="text" name="n"/>
+			<button type="submit">Compute GCD</button>
+			</form>
+		"#);
+		Ok(response)
+	}
