4cefc2d93bff26d69643290a7bb02b15b26e5657
Konstantin Perikov <pericov.ko@gmail.com>
2017-07-06 11:38:56 +0300

    fix mime type

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-extern crate iron;
+#[macro_use]
 extern crate mime;
+extern crate iron;
+
+use std::env;
 
 use iron::prelude::*;
+use iron::headers::ContentType;
 use iron::status;
 
 	fn main() {
@@ -9,17 +13,19 @@ use iron::status;
 		Iron::new(get_form).http("localhost:3000").unwrap();
 	}
 
-	#[allow(unused_variables)]
-	fn get_form(request: &mut Request) -> IronResult<Response> {
-		let mut response = Response::new();
-		response.set_mut(status::Ok);
-		response.set_mut(r#"
+	fn get_form(_: &mut Request) -> IronResult<Response> {
+		use iron::mime;
+		let content_type = "text/html".parse::<mime::Mime>().unwrap();
+		Ok(Response::with(
+			(content_type,
+			 status::Ok,
+			 r#"
 			<title>GCD Calculator</title>
-			<form action="/gcd" method="post">
-			<input type="text" name="n"/>
-			<input type="text" name="n"/>
-			<button type="submit">Compute GCD</button>
-			</form>
-		"#);
-		Ok(response)
+                        <form action="/gcd" method="post">
+                        <input type="text" name="n"/>
+                        <input type="text" name="n"/>
+                        <button type="submit">Compute GCD</button>
+                        </form>
+			"#)
+		))
 	}
