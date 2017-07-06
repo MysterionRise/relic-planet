29ff737e546cd1840876151ebb9afc5f371a8ff0
Konstantin Perikov <pericov.ko@gmail.com>
2017-07-06 17:21:53 +0300

    fix the gcd http server

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,100 @@
-#[macro_use]
 extern crate mime;
 extern crate iron;
+extern crate router;
+extern crate urlencoded;
 
-use std::env;
-
+use router::Router;
 use iron::prelude::*;
-use iron::headers::ContentType;
 use iron::status;
+use std::str::FromStr;
+use urlencoded::UrlEncodedBody;
 
-	fn main() {
-		println!("Serving on http://localhost:3000...");
-		Iron::new(get_form).http("localhost:3000").unwrap();
-	}
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
+}
 
-	fn get_form(_: &mut Request) -> IronResult<Response> {
-		use iron::mime;
-		let content_type = "text/html".parse::<mime::Mime>().unwrap();
-		Ok(Response::with(
-			(content_type,
-			 status::Ok,
-			 r#"
+fn main() {
+    let mut router = Router::new();
+
+    router.get("/", get_form, "index");
+    router.post("/gcd", post_gcd, "gcd");
+
+    println!("Serving on http://localhost:3000...");
+
+    Iron::new(router).http("localhost:3000").unwrap();
+}
+
+fn post_gcd(req: &mut Request) -> IronResult<Response> {
+    let mut response = Response::new();
+
+    match req.get_ref::<UrlEncodedBody>() {
+        Err(ex) => {
+            response.set_mut(status::BadRequest);
+            response.set_mut(format!("Error parsing form: {:?}\n", ex));
+            return Ok(response);
+        }
+        Ok(map) => {
+            match map.get("n") {
+                None => {
+                    response.set_mut(status::BadRequest);
+                    response.set_mut(format!("Form data has no 'n' parameter"));
+                    return Ok(response);
+                }
+                Some(numbers) => {
+                    let mut arr = Vec::new();
+                    for n in numbers {
+                        match u64::from_str(&n) {
+                            Err(_) => {
+                                response.set_mut(status::BadRequest);
+                                response.set_mut(format!("Value for 'n' is not a number: {:?}", n));
+                                return Ok(response);
+                            }
+                            Ok(parsed) => {
+                                arr.push(parsed);
+                            }
+                        }
+                    }
+                    let mut d = arr[0];
+                    for m in &arr[1..] {
+                        d = gcd(d, *m);
+                    }
+                    response.set_mut(status::Ok);
+                    response.set_mut(format!(
+                        "The greatest common divisor of the numbers {:?} is {}",
+                        arr,
+                        d
+                    ));
+                }
+            }
+        }
+
+    }
+
+    Ok(response)
+}
+
+fn get_form(_: &mut Request) -> IronResult<Response> {
+    use iron::mime;
+    let content_type = "text/html".parse::<mime::Mime>().unwrap();
+    Ok(Response::with((
+        content_type,
+        status::Ok,
+        r#"
 			<title>GCD Calculator</title>
                         <form action="/gcd" method="post">
                         <input type="text" name="n"/>
                         <input type="text" name="n"/>
                         <button type="submit">Compute GCD</button>
                         </form>
-			"#)
-		))
-	}
+			"#,
+    )))
+}
